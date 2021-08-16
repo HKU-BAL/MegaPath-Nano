a9cee88b535435cee0a433909b331c3bbdffc7af
Ed Lui <rewq142857@hotmail.com>
2021-08-17 00:44:56 +0800

    Update addDecoyDB.py

--- db_preparation/addDecoyDB.py
+++ db_preparation/addDecoyDB.py
@@ -8,26 +8,27 @@ if __name__ == '__main__':
     parser.add_argument('--decoy_fasta', required=True)
 
     cwd=os.path.dirname(os.path.realpath(__file__))
-    nano_dir=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    parser.add_argument('--assembly_dir', default='%s/genomes' %(nano_dir))
-    parser.add_argument('--config_folder', help='Config file folder', default='%s/config' %(nano_dir))
+    NANO_DIR=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    parser.add_argument('--assembly_dir', default=f'{NANO_DIR}/genomes' )
+    parser.add_argument('--config_folder', help='Config file folder', default=f'{NANO_DIR}/config' )
 
     FLAGS = parser.parse_args()
 
-    decoy_name=os.path.basename(os.path.splitext(FLAGS.decoy_fasta))
-    path= '%s/refseq/plasmid/%s' %(assembly_dir,decoy_name)
+    decoy_name=os.path.basename(os.path.splitext(FLAGS.decoy_fasta)[0])
+    path= f'{FLAGS.assembly_dir}/refseq/plasmid/{decoy_name}.fa'
     copyfile(FLAGS.decoy_fasta,path)
-    with open('%s/plasmid.genome_set' %(FLAGS.config_folder),'a') as config_plasmid:
-        config_plasmid.write(decoy_name+'\n')
-    assemblyLengthWriter = open('%s/assembly_length' %(FLAGS.assembly_dir), 'a')
-    assemblyPathWriter = open('%s/assembly_path' %(FLAGS.assembly_dir), 'a')
-    assemblyTaxidWriter = open('%s/assembly_tax_id' %(FLAGS.assembly_dir), 'a')
-    sequenceSummaryWriter = open('%s/sequence_summary' %(FLAGS.assembly_dir), 'a')
+    with open(f'{FLAGS.config_folder}/plasmid.genome_set' ,'a') as config_plasmid:
+        config_plasmid.write(f'{decoy_name}\n')
+    assemblyLengthWriter = open(f'{FLAGS.assembly_dir}/assembly_length' , 'a')
+    assemblyPathWriter = open(f'{FLAGS.assembly_dir}/assembly_path' , 'a')
+    assemblyTaxidWriter = open(f'{FLAGS.assembly_dir}/assembly_tax_id' , 'a')
+    sequenceSummaryWriter = open(f'{FLAGS.assembly_dir}/sequence_summary' , 'a')
+    totalLength=0
     with open(path, 'rt') as fi:
         for record in SeqIO.parse(fi, 'fasta'):
             totalLength += len(record)
-            sequenceSummaryWriter.write("%s\t%d\t%s\n"%(record.id, len(record), decoy_name))
-    assemblyLengthWriter.write("%s\t%d\n"%(decoy_name, totalLength))
-    assemblyPathWriter.write("%s\t%s\n"%(decoy_name, path))
-    assemblyTaxidWriter.write("%s\t1000000099\t1000000099\t1000000001\t%s\n"%(decoy_name, '35'))  #arbitrary taxid
-
+            sequenceSummaryWriter.write(f"{record.id}\t{len(record)}\t{decoy_name}\n")
+    assemblyLengthWriter.write(f"{decoy_name}\t{totalLength}\n")
+    assemblyPathWriter.write(f"{decoy_name}\t{path}\n")
+    arbitrary_taxid='35'
+    assemblyTaxidWriter.write(f"{decoy_name}\t1000000099\t1000000099\t1000000001\t{arbitrary_taxid}\n")
